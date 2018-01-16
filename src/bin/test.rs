69a789ebf6f23f23aca2d1a7b4fb9bd55baca438
Katharina Fey <kookie@spacekookie.de>
2018-01-16 02:04:38 +0100

    Adjusting the test to use new API

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -2,9 +2,16 @@ extern crate barrel;
 use barrel::*;
 
 fn main() {
-    let mut s = Schema::name("public").create_table("users", |t: &mut Table| {
+    
+    let mut s = Schema::new().with_schema("public").create_table("users", |t: &mut Table| {
         t.increments();
     });
 
     println!("{}", s.exec());
+
+    // Schema::create_table("users", |t| {
+
+    // });
+
+    // Schema::with_schema("public").create_table
 }
