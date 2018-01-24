448b27c362fa8075dfacb30d15377560086b9536
Katharina Fey <kookie@spacekookie.de>
2018-01-24 21:02:36 +0100

    Working on the table generators

--- src/backends/postgres.rs
+++ src/backends/postgres.rs
@@ -18,30 +18,166 @@ impl DatabaseGenerator for PGSQL {
     fn create_table(name: &str) -> String {
         return format!("create table {}", name);
     }
-
     
     fn create_table_if_not_exists(name: &str) -> String {
         return format!("create table if not exists {}", name);
     }
-
     
     fn drop_table(name: &str) -> String {
         return format!("drop table {}", name);
     }
-
     
     fn drop_table_if_exists(name: &str) -> String {
         return format!("drop table if exists {}", name);
     }
-
     
     fn rename_table(old: &str, new: &str) -> String {
         return format!("alter table {} rename to {}", old, new);
     }
-
     
     fn modify_table(name: &str) -> String {
         return format!("alter table {}", name);
     }
+
+}
+
+impl TableGenerator for PGSQL {
+
+    fn dropColumn(&mut self, name: &str) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn renameColumn(&mut self, old: &str, new: &str) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn increments(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn integer(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn bigInteger(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn text(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn string(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn float(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn decimal(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn boolean(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn date(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn dateTime(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn time(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn timestamp(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn timestamps(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn dropTimestamps(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn binary(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn enumerable(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn json(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn jsonb(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn uuid(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn comment(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn engine(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn charset(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn collate(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn inherits(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn specificType(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn index(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn dropIndex(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn unique(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn foreign(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn dropForeign(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn dropUnique(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
+    fn dropPrimary(&mut self) -> TableGenerated {
+        unimplemented!();
+    }
+
 
 }
--- src/traits.rs
+++ src/traits.rs
@@ -29,18 +29,50 @@ pub trait DatabaseGenerator {
 }
 
 /// A partially generated snippet of a table manipulation
-pub struct TableGenerated<T> {
-    name: String,
-    value: T
+pub struct TableGenerated {
+    pub name: String,
+    // value: T
 }
 
 
 /// A module which generates SQL syntax foused around generating
 /// table manipulation statements
 pub trait TableGenerator {
-    
-    /// Add an auto incrementing primary key
-    fn increments<T>() -> TableGenerated<T>;
+
+    fn dropColumn(&mut self, name: &str) -> TableGenerated;
+    fn renameColumn(&mut self, old: &str, new: &str) -> TableGenerated;
+    fn increments(&mut self) -> TableGenerated;
+    fn integer(&mut self) -> TableGenerated;
+    fn bigInteger(&mut self) -> TableGenerated;
+    fn text(&mut self) -> TableGenerated;
+    fn string(&mut self) -> TableGenerated;
+    fn float(&mut self) -> TableGenerated;
+    fn decimal(&mut self) -> TableGenerated;
+    fn boolean(&mut self) -> TableGenerated;
+    fn date(&mut self) -> TableGenerated;
+    fn dateTime(&mut self) -> TableGenerated;
+    fn time(&mut self) -> TableGenerated;
+    fn timestamp(&mut self) -> TableGenerated;
+    fn timestamps(&mut self) -> TableGenerated;
+    fn dropTimestamps(&mut self) -> TableGenerated;
+    fn binary(&mut self) -> TableGenerated;
+    fn enumerable(&mut self) -> TableGenerated;
+    fn json(&mut self) -> TableGenerated;
+    fn jsonb(&mut self) -> TableGenerated;
+    fn uuid(&mut self) -> TableGenerated;
+    fn comment(&mut self) -> TableGenerated;
+    fn engine(&mut self) -> TableGenerated;
+    fn charset(&mut self) -> TableGenerated;
+    fn collate(&mut self) -> TableGenerated;
+    fn inherits(&mut self) -> TableGenerated;
+    fn specificType(&mut self) -> TableGenerated;
+    fn index(&mut self) -> TableGenerated;
+    fn dropIndex(&mut self) -> TableGenerated;
+    fn unique(&mut self) -> TableGenerated;
+    fn foreign(&mut self) -> TableGenerated;
+    fn dropForeign(&mut self) -> TableGenerated;
+    fn dropUnique(&mut self) -> TableGenerated;
+    fn dropPrimary(&mut self) -> TableGenerated;
 
     
 
