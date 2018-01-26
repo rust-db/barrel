c6c5b6b262d1222c34c4a5da3ec4cc0d7949fa9a
Katharina Fey <kookie@spacekookie.de>
2018-01-26 02:56:57 +0100

    Cleaning up tests and fixing minor incompatibilities

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,12 +1,23 @@
 extern crate barrel;
-use barrel::traits::*;
-use barrel::*;
 
+use barrel::{Schema, Table};
+use barrel::generators::postgres::*;
 
 fn main() {
 
-    let mut s = Schema::new().create_table("users", |t: &mut Table| {
+    println!("Test!");
+
+    let mut sql = Schema::<PGSQL>::new();
+    sql.create_table("users", |t: &mut Table<PGSQL>| {
         t.increments();
+        t.string("username");
+        t.integer("plushy_sharks_owned");
     });
+
+
+        t.timestamp("joined");
+        t.timestamp("birthday");
+
+    println!("{}", sql.exec());
 
 }
--- src/test/schema.rs
+++ src/test/schema.rs
@@ -2,15 +2,15 @@
 
 use schema::Schema;
 
-fn test_stuff() {
+// fn test_stuff() {
 
-    let mut schema = Schema::new();
-    schema.create_table("users", |_| {});
-    schema.create_table_if_not_exists("users", |_| {});
-    schema.rename_table("users", "zombies");
-    schema.drop_table("users");
-    schema.drop_table_if_exists("users");
-    schema.table("users", |_| {});
+//     let mut schema = Schema::new();
+//     schema.create_table("users", |_| {});
+//     schema.create_table_if_not_exists("users", |_| {});
+//     schema.rename_table("users", "zombies");
+//     schema.drop_table("users");
+//     schema.drop_table_if_exists("users");
+//     schema.table("users", |_| {});
 
-    println!("{}", schema.exec());
-}
+//     println!("{}", schema.exec());
+// }
