63b4001a913d2489fcb3d8d8b2fc5a2f45fd9978
Katharina Fey <kookie@spacekookie.de>
2018-02-14 02:39:55 +0100

    Fixing the pg module

--- src/backend/pg.rs
+++ src/backend/pg.rs
@@ -5,27 +5,27 @@ use super::{SqlGenerator, Type};
 pub struct Pg;
 impl SqlGenerator for Pg {
     fn create_table(name: &str) -> String {
-        return format!("CREATE TABLE {}", name);
+        return format!("CREATE TABLE \"{}\"", name);
     }
 
     fn create_table_if_not_exists(name: &str) -> String {
-        return format!("CREATE TABLE {} IF NOT EXISTS", name);
+        return format!("CREATE TABLE \"{}\" IF NOT EXISTS", name);
     }
 
     fn drop_table(name: &str) -> String {
-        return format!("DROP TABLE {}", name);
+        return format!("DROP TABLE \"{}\"", name);
     }
 
     fn drop_table_if_exists(name: &str) -> String {
-        return format!("DROP TABLE {} IF EXISTS", name);
+        return format!("DROP TABLE \"{}\" IF EXISTS", name);
     }
 
     fn rename_table(old: &str, new: &str) -> String {
-        return format!("ALTER TABLE {} RENAME TO {}", old, new);
+        return format!("ALTER TABLE \"{}\" RENAME TO \"{}\"", old, new);
     }
 
     fn alter_table(name: &str) -> String {
-        return format!("ALTER TABLE {}", name);
+        return format!("ALTER TABLE \"{}\"", name);
     }
 
     fn add_column(name: &str, _type: Type) -> String {
@@ -53,11 +53,11 @@ impl SqlGenerator for Pg {
     }
 
     fn drop_column(name: &str) -> String {
-        return format!("DROP COLUMN {}", name);
+        return format!("DROP COLUMN \"{}\"", name);
     }
 
     fn rename_column(old: &str, new: &str) -> String {
-        return format!("ALTER COLUMN {} RENAME TO {}", old, new);
+        return format!("ALTER COLUMN \"{}\" RENAME TO \"{}\"", old, new);
     }
 }
 
