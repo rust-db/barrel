c8d6356b811ff5823cc57f183ee922e8ddb5330f
Katharina Fey <kookie@spacekookie.de>
2018-02-14 22:48:09 +0100

    Oups, now it's fixed

--- src/tests/pg/add_column.rs
+++ src/tests/pg/add_column.rs
@@ -6,96 +6,96 @@ use Type::*;
 
 #[test]
 fn text() {
-    let sql = Pg::add_column("Text", Text);
-    assert_eq!(String::from("\"Text\" TEXT"), sql);
+    let sql = Pg::add_column(true, "Text", &Text);
+    assert_eq!(String::from("ADD COLUMN \"Text\" TEXT"), sql);
 }
 
 #[test]
 fn varchar() {
-    let sql = Pg::add_column("Varchar", Varchar(255));
-    assert_eq!(String::from("\"Varchar\" VARCHAR(255)"), sql);
+    let sql = Pg::add_column(true, "Varchar", &Varchar(255));
+    assert_eq!(String::from("ADD COLUMN \"Varchar\" VARCHAR(255)"), sql);
 }
 
 #[test]
 fn integer() {
-    let sql = Pg::add_column("Integer", Integer);
-    assert_eq!(String::from("\"Integer\" INTEGER"), sql);
+    let sql = Pg::add_column(true, "Integer", &Integer);
+    assert_eq!(String::from("ADD COLUMN \"Integer\" INTEGER"), sql);
 }
 
 #[test]
 fn float() {
-    let sql = Pg::add_column("Float", Float);
-    assert_eq!(String::from("\"Float\" FLOAT"), sql);
+    let sql = Pg::add_column(true, "Float", &Float);
+    assert_eq!(String::from("ADD COLUMN \"Float\" FLOAT"), sql);
 }
 
 #[test]
 fn double() {
-    let sql = Pg::add_column("Double", Double);
-    assert_eq!(String::from("\"Double\" DOUBLE"), sql);
+    let sql = Pg::add_column(true, "Double", &Double);
+    assert_eq!(String::from("ADD COLUMN \"Double\" DOUBLE"), sql);
 }
 
 #[test]
 fn boolean() {
-    let sql = Pg::add_column("Boolean", Boolean);
-    assert_eq!(String::from("\"Boolean\" BOOLEAN"), sql);
+    let sql = Pg::add_column(true, "Boolean", &Boolean);
+    assert_eq!(String::from("ADD COLUMN \"Boolean\" BOOLEAN"), sql);
 }
 
 #[test]
 fn binary() {
-    let sql = Pg::add_column("Binary", Binary);
-    assert_eq!(String::from("\"Binary\" BINARY"), sql);
+    let sql = Pg::add_column(true, "Binary", &Binary);
+    assert_eq!(String::from("ADD COLUMN \"Binary\" BINARY"), sql);
 }
 
 #[test]
 fn foreign() {
-    let sql = Pg::add_column("Foreign", Foreign("posts"));
-    assert_eq!(String::from("\"Foreign\" INTEGER REFERENCES posts"), sql);
+    let sql = Pg::add_column(true, "Foreign", &Foreign("posts"));
+    assert_eq!(String::from("ADD COLUMN \"Foreign\" INTEGER REFERENCES posts"), sql);
 }
 
 #[test]
 fn array_text() {
-    let sql = Pg::add_column("Array of Text", Array(box Text));
-    assert_eq!(String::from("\"Array of Text\" TEXT[]"), sql);
+    let sql = Pg::add_column(true, "Array of Text", &Array(box Text));
+    assert_eq!(String::from("ADD COLUMN \"Array of Text\" TEXT[]"), sql);
 }
 
 #[test]
 fn array_varchar() {
-    let sql = Pg::add_column("Array of Varchar", Array(box Varchar(255)));
-    assert_eq!(String::from("\"Array of Varchar\" VARCHAR(255)[]"), sql);
+    let sql = Pg::add_column(true, "Array of Varchar", &Array(box Varchar(255)));
+    assert_eq!(String::from("ADD COLUMN \"Array of Varchar\" VARCHAR(255)[]"), sql);
 }
 
 #[test]
 fn array_integer() {
-    let sql = Pg::add_column("Array of Integer", Array(box Integer));
-    assert_eq!(String::from("\"Array of Integer\" INTEGER[]"), sql);
+    let sql = Pg::add_column(true, "Array of Integer", &Array(box Integer));
+    assert_eq!(String::from("ADD COLUMN \"Array of Integer\" INTEGER[]"), sql);
 }
 
 #[test]
 fn array_float() {
-    let sql = Pg::add_column("Array of Float", Array(box Float));
-    assert_eq!(String::from("\"Array of Float\" FLOAT[]"), sql);
+    let sql = Pg::add_column(true, "Array of Float", &Array(box Float));
+    assert_eq!(String::from("ADD COLUMN \"Array of Float\" FLOAT[]"), sql);
 }
 
 #[test]
 fn array_double() {
-    let sql = Pg::add_column("Array of Double", Array(box Double));
-    assert_eq!(String::from("\"Array of Double\" DOUBLE[]"), sql);
+    let sql = Pg::add_column(true, "Array of Double", &Array(box Double));
+    assert_eq!(String::from("ADD COLUMN \"Array of Double\" DOUBLE[]"), sql);
 }
 
 #[test]
 fn array_boolean() {
-    let sql = Pg::add_column("Array of Boolean", Array(box Boolean));
-    assert_eq!(String::from("\"Array of Boolean\" BOOLEAN[]"), sql);
+    let sql = Pg::add_column(true, "Array of Boolean", &Array(box Boolean));
+    assert_eq!(String::from("ADD COLUMN \"Array of Boolean\" BOOLEAN[]"), sql);
 }
 
 #[test]
 fn array_binary() {
-    let sql = Pg::add_column("Array of Binary", Array(box Binary));
-    assert_eq!(String::from("\"Array of Binary\" BINARY[]"), sql);
+    let sql = Pg::add_column(true, "Array of Binary", &Array(box Binary));
+    assert_eq!(String::from("ADD COLUMN \"Array of Binary\" BINARY[]"), sql);
 }
 
 #[test]
 fn array_array_integer() {
-    let sql = Pg::add_column("Array of Array of Integer", Array(box Array(box Integer)));
-    assert_eq!(String::from("\"Array of Array of Integer\" INTEGER[][]"), sql);
+    let sql = Pg::add_column(true, "Array of Array of Integer", &Array(box Array(box Integer)));
+    assert_eq!(String::from("ADD COLUMN \"Array of Array of Integer\" INTEGER[][]"), sql);
 }
