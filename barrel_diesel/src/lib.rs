d27cacdb9c44226567c67882df7ba081f1a63aeb
Katharina Fey <kookie@spacekookie.de>
2018-02-15 16:22:28 +0100

    Trying out this new structure

--- barrel_diesel/src/lib.rs
+++ barrel_diesel/src/lib.rs
@@ -1,6 +1,11 @@
-//!
+//! A compatibility crate between barrel and the diesel ORM
+//! 
+//! 
 
 extern crate diesel;
+extern crate barrel;
+
+use barrel::*;
 
 pub fn test() {
     println!("Wazzap!");
