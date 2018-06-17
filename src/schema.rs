e1820858fa55b0dc7d8f0d1664b7a7fc24e5c456
Katharina Fey <kookie@spacekookie.de>
2018-06-17 23:04:10 +0200

    Working on the new schema module

--- src/schema.rs
+++ src/schema.rs
@@ -1,11 +1,22 @@
 //! Simple schema representation for migration state
 
 trait Schemas {
-    
+    /// Returns the name of a database
+    fn name(&self) -> String;
+
+    /// Returns a list of all tables in a database
+    fn tables(&self) -> Vec<String>;
+
+    /// Returns a list of all column names and types
+    fn columns(&self, table: &str) -> Vec<(String, Column)>;
 }
 
+trait Column {
+    /// Get the type of column in SQL specific terms
+    fn type(&self) -> String;
+}
 
-/// Describe the current state of a database to apply a migration to
+// Describe the current state of a database to apply a migration to
 struct Schema {
     db_name: String,
     columns: Vec<String>,
@@ -16,8 +27,3 @@ impl Schema {
 
 }
 
-
-src/
-  foo.rs
-  foo/
-    bar.rs
