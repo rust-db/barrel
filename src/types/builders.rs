e9b07ee8874a841d37bacc770c95f6415614d675
Katharina Fey <kookie@spacekookie.de>
2018-08-14 13:50:16 +0200

    Adding skeleton builder functions

--- src/types/builders.rs
+++ src/types/builders.rs
@@ -2,6 +2,52 @@
 
 use super::Type;
 
-pub fn number() -> Type {
+///
+pub fn integer() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn float() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn double() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn boolean() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn varchar() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn text() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn json() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn binary() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn foreign() -> Type {
+    unimplemented!()
+}
+
+///
+pub fn array() -> Type {
     unimplemented!()
 }
