d2523bc67ed99c400b8bbdafc3a69c0ad54873a8
Rob Rowe <rrowe@foodlogiq.com>
2019-03-04 17:28:23 -0500

    merged the CreateTable and CreateTableIfNotExists arms in the make method per PR feedback, removed an unnecessary println call

--- src/migration.rs
+++ src/migration.rs
@@ -55,25 +55,7 @@ impl Migration {
         let mut changes = self.changes.clone();
         for change in &mut changes {
             match change {
-                &mut CreateTable(ref mut t, ref mut cb) => {
-                    if t.meta.has_id {
-                        t.add_column("id", types::primary());
-                    }
-
-                    cb(t); // Run the user code
-                    let vec = t.make::<T>(false);
-                    s.push_str(&T::create_table(&t.meta.name()));
-                    s.push_str(" (");
-                    let l = vec.len();
-                    for (i, slice) in vec.iter().enumerate() {
-                        s.push_str(slice);
-
-                        if i < l - 1 {
-                            s.push_str(", ");
-                        }
-                    }
-                    s.push_str(")");
-                },
+                &mut CreateTable(ref mut t, ref mut cb) |
                 &mut CreateTableIfNotExists(ref mut t, ref mut cb) => {
                     if t.meta.has_id {
                         t.add_column("id", types::primary());
@@ -81,7 +63,11 @@ impl Migration {
 
                     cb(t); // Run the user code
                     let vec = t.make::<T>(false);
-                    s.push_str(&T::create_table_if_not_exists(&t.meta.name()));
+                    s.push_str( &match change {
+                        CreateTable(_, _) => T::create_table(&t.meta.name()),
+                        CreateTableIfNotExists(_, _) => T::create_table_if_not_exists(&t.meta.name()),
+                        _ => unreachable!()
+                    });
                     s.push_str(" (");
                     let l = vec.len();
                     for (i, slice) in vec.iter().enumerate() {
@@ -93,7 +79,6 @@ impl Migration {
                     }
                     s.push_str(")");
                 },
-
                 &mut DropTable(ref name) => s.push_str(&T::drop_table(name)),
                 &mut DropTableIfExists(ref name) => s.push_str(&T::drop_table_if_exists(name)),
                 &mut RenameTable(ref old, ref new) => s.push_str(&T::rename_table(old, new)),
@@ -122,7 +107,7 @@ impl Migration {
 
     /// Automatically infer the `down` step of this migration
     ///
-    /// Will thrown an error if behaviour is ambigous or not
+    /// Will thrown an error if behaviour is ambiguous or not
     /// possible to infer (e.g. revert a `drop_table`)
     pub fn revert<T: SqlGenerator>(&self) -> String {
         unimplemented!()
