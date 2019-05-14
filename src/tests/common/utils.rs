a6d13ff6718558ea546c0b0a27efb89e339a89a7
Katharina Fey <kookie@spacekookie.de>
2019-05-14 18:30:16 +0200

    Changing a test to be in-line with the new Debug output

--- src/tests/common/utils.rs
+++ src/tests/common/utils.rs
@@ -9,7 +9,7 @@ fn cloning_types() {
 #[test]
 fn debugging_types() {
     let tt = types::text();
-    assert_eq!(format!("{:#?}", tt), "Type {\n    nullable: false,\n    unique: false,\n    increments: false,\n    indexed: false,\n    default: None,\n    size: None,\n    inner: Text\n}".to_owned());
+    assert_eq!(format!("{:#?}", tt), "Type {\n    nullable: false,\n    unique: false,\n    increments: false,\n    indexed: false,\n    default: None,\n    size: None,\n    inner: Text,\n}".to_owned());
 }
 
 #[test]
