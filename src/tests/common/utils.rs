955d1baf5091a5e568787e8f7aadf677f2b8f14e
Katharina Fey <kookie@spacekookie.de>
2019-05-14 19:37:42 +0200

    Removing the `debugging_types` test
    This test was meant to make sure that the internal layout and
    representation of a type doesn't change. Since we aren't actually
    stable yet (i.e. `1.0.0`) this isn't really required or a good
    idea. There've been a few breakages recently because of this test and
    it's getting annoying to debug.
    
    So we're removing it for now. Maybe at some point in the future we can
    add another type of tests that does a similar thing again...

--- src/tests/common/utils.rs
+++ src/tests/common/utils.rs
@@ -7,12 +7,6 @@ fn cloning_types() {
 }
 
 #[test]
-fn debugging_types() {
-    let tt = types::text();
-    assert_eq!(format!("{:#?}", tt), "Type {\n    nullable: false,\n    unique: false,\n    increments: false,\n    indexed: false,\n    default: None,\n    size: None,\n    inner: Text,\n}".to_owned());
-}
-
-#[test]
 fn equals_types() {
     let t1 = types::text();
     let t2 = t1.clone();
