0a12760e36de77a2e0ce3f0746226477032a7c26
David Karrick <the.white.wolf.is.1337@gmail.com>
2021-03-11 02:27:21 +0000

    Do not quote default values for Pg backend
    Single-quoting the value makes it impossible to use functions. Instead, the quoting should happen in the parameter to default()

--- src/backend/pg.rs
+++ src/backend/pg.rs
@@ -90,7 +90,7 @@ impl SqlGenerator for Pg {
                     WrappedDefault::Null => format!(" DEFAULT NULL"),
                     WrappedDefault::AnyText(ref val) => format!(" DEFAULT '{}'", val),
                     WrappedDefault::UUID(ref val) => format!(" DEFAULT '{}'", val),
-                    WrappedDefault::Date(ref val) => format!(" DEFAULT '{:?}'", val),
+                    WrappedDefault::Date(ref val) => format!(" DEFAULT {:?}", val),
                     WrappedDefault::Custom(ref val) => format!(" DEFAULT '{}'", val),
                     _ => format!(" DEFAULT {}", m)
                 },
